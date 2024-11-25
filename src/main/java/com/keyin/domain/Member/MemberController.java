6fb0151e9d9990a74c31b183384682a4a51e3843
ifiyemiO <ifiyemi.owi@keyin.com>
2024-11-25 08:22:34 -0330

    updated

--- src/main/java/com/keyin/domain/Member/MemberController.java
+++ src/main/java/com/keyin/domain/Member/MemberController.java
@@ -1,4 +1,29 @@
 package com.keyin.domain.Member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/members")
 public class MemberController {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @PostMapping
+    public Member addMember(@RequestBody Member member) {
+        return memberRepository.save(member);
+    }
+
+    @GetMapping
+    public List<Member> getAllMembers() {
+        return memberRepository.findAll();
+    }
+
+    @GetMapping("/search")
+    public List<Member> searchMembers(@RequestParam String name) {
+        return memberRepository.findByName(name);
+    }
 }
+
--- src/main/java/com/keyin/domain/Member/MemberRepository.java
+++ src/main/java/com/keyin/domain/Member/MemberRepository.java
@@ -1,4 +1,10 @@
 package com.keyin.domain.Member;
 
-public class MemberRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByName(String name);
+    List<Member> findByPhoneNumber(String phoneNumber);
 }
